d1914c95b0a86128dc7948d55a6abe900e49d5fa
Shiiva03 <unchart6565@gmail.com>
2021-03-16 16:23:52 +0000

    Finished day2 in Haskell

--- Day2/day2.hs
+++ Day2/day2.hs
@@ -2,8 +2,9 @@ module Main where
 
 import System.Environment
 import Control.Monad
-import Data.Set hiding (map,take)
+import Data.Set hiding (map,take,null)
 import Data.List.Split
+import Data.List
 
 
 main :: IO()
@@ -11,7 +12,9 @@ main = do
         f <- readFile "passInput"
         let inputs =  lines f
             howmany = sum $ map ( fromEnum . checkValid . parseInput) inputs
+            howmany2 = sum $ map ( fromEnum . checkValid2 .parseInput) inputs        
         print(show howmany)
+        print(show howmany2)
          
 data Info = Info{
     min :: Int,
@@ -32,3 +35,13 @@ checkValid :: Info -> Bool
 checkValid (Info min max lett pass) = len >= min && len <= max
     where len = length[x | x <- pass , x == lett]
 
+unJust :: Maybe x -> x
+unJust (Just x) = x
+
+checkValid2 :: Info -> Bool
+checkValid2 (Info min max lett pass) | null index = False  
+                                     | elem min list /= elem max list = True
+                                     | otherwise = False
+    where index = elemIndices lett pass
+          list  = map (+1) index
+          
